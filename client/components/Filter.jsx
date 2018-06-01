013a7cf301d67e2ffbbe4cadeacb92805291ef74
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-01 10:20:56 -0700

    Connected components with main App

---
+++ client/components/Filter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Filter extends React.Component {
+  render () {
+    return (
+      <div>Filter stuff goes here</div>
+    );
+  }
+}
+
+export default Filter;
---
+++ client/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div> This is the landing stuff </div>
+    );
+  }
+}
+
+export default Landing;
