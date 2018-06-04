a03066cf8634a50a5fcbb8e675cb4023c2ccf848
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-03 18:44:18 -0700

    Added tests for Organization schema and Opportunities schema

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,25 @@
 const expect = require('chai').expect;
 const request = require('request');
-const db = require('../database/db.js');
-const dbConnection = require('../database/db.js').dbConnection;
 const app = require('../server/index.js');
-const VolunteerSchema = db.Volunteers.schema.obj;
+const models = require('../database/models.js');
+const VolunteerSchema = models.Volunteers.schema.obj;
+const OrganizationSchema = models.Organizations.schema.obj;
+const OpportunitiesSchema = models.Opportunities.schema.obj;
+var server;
+
+before(function() {
+  server = app.listen(4568, function() {
+    console.log('The tests are listening on 4568');
+  });
+});
+
+after(function() {
+  server.close();
+});
 
 describe ('Database', function() {
 
-  var server;
-
-  before(function() {
-    server = app.listen(4568, function() {
-      console.log('Shortly is listening on 4568');
-    });
-  });
-
-  after(function() {
-    dbConnection.close();
-    server.close();
-  });
-
-  describe('Volunteer', function() {
+  describe('Volunteers', function() {
 
     describe('Volunteer Schema', function() {
 
@@ -41,7 +40,63 @@ describe ('Database', function() {
     
   });
 
-}); 
+  describe('Organizations', function() {
 
-console.log(app)
+    describe('Organization Schema', function() {
+ 
+      it('Should have a name property', function() {
+        expect(OrganizationSchema.name).to.exist;
+      });
+      it('Should have a address property', function() {
+        expect(OrganizationSchema.address).to.exist;
+      });
+      it('Should have a phone property', function() {
+        expect(OrganizationSchema.phone).to.exist;
+      });
+      it('Should have a email property', function() {
+        expect(OrganizationSchema.email).to.exist;
+      });
+
+    });
+
+  });
+
+  describe('Opportunitiess', function() {
+
+    describe('Opportunity Schema', function() {
+ 
+      it('Should have a title property', function() {
+        expect(OpportunitiesSchema.title).to.exist;
+      });
+      it('Should have a description property', function() {
+        expect(OpportunitiesSchema.description).to.exist;
+      });
+      it('Should have a cause property', function() {
+        expect(OpportunitiesSchema.cause).to.exist;
+      });
+      it('Should have a address property', function() {
+        expect(OpportunitiesSchema.address).to.exist;
+      });
+      it('Should have a start date property', function() {
+        expect(OpportunitiesSchema.start_date).to.exist;
+      });
+      it('Should have a end date property', function() {
+        expect(OpportunitiesSchema.end_date).to.exist;
+      });
+      it('Should have a phone property', function() {
+        expect(OpportunitiesSchema.phone).to.exist;
+      });
+      it('Should have a email property', function() {
+        expect(OpportunitiesSchema.email).to.exist;
+      });
+
+
+
+
+
+    });
+
+  });
+
+}); 
 
