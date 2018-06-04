0a8bb428fa725396b964d4494a1338468770ff2c
Matthew Choi <mattjchoi@gmail.com>
2018-06-04 12:36:23 -0700

    Restructure App/Index.

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         loader: 'style-loader!css-loader'
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
         use: [
           'file-loader',
           {
