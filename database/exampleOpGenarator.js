8e723d57c28c9aa613fca597152f3c2f5f178cfd
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-09 15:49:42 -0700

    Fixed googleOauth again and added volunteerer array to exampleOpGen

--- database/exampleOpGenarator.js
+++ database/exampleOpGenarator.js
@@ -7,7 +7,7 @@ const saveToDb = require('./saveToDb.js');
 
 function saveExampleOpportunity() {
   const opportunities = {};
-  for(var i = 0; i < rrad.addresses.length ; i++) {
+  for(var i = 0; i < 2200; i++) {
     const randomAddress = rrad.addresses[Math.floor(Math.random() * rrad.addresses.length)];
     opportunities['title'] = "Community Service";
     opportunities['description']="Help make the world a better place";
@@ -17,6 +17,7 @@ function saveExampleOpportunity() {
     opportunities['end_date']='07/09/2018';
     opportunities['phone']= '1234567890';
     opportunities['email']= 'help@helpwanted.com';
+    opportunities['volunteerers'] = [];
 
     console.log(opportunities);
     //saveToDb.newOpportunity(opportunities);
