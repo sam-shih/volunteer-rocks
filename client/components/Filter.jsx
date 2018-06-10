2ab04e7b074a11eaa91235b959f2368ca5eb402d
Linda Wang <lindawang256@gmail.com>
2018-06-10 08:43:06 -0700

    add filter component

--- client/components/Filter.jsx
+++ client/components/Filter.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, NavLink, Input, Form, FormGroup, Label, Col } from 'reactstrap';
+import _ from 'underscore';
 
 class Filter extends React.Component {
-  render () {
-    return (
-      <div>Filter stuff goes here</div>
-    );
+  constructor (props){
+    super(props);
+    this.updateInput = this.updateInput.bind(this);
+
+  this.state = {
+    currFilter: {
+    cause:[],
+    start_date:'',
+    end_date:''
+    }
   }
 }
+
+  updateInput(e) {
+    console.log(e.target.value);
+    const currFilter = this.state.currFilter; // Copy current state to object 'form'
+    currFilter[e.target.name] = e.target.value; // Set  key:value to form
+    this.setState({ currFilter: currFilter });
+  }
+
+  render () {
+
+var causes = [];
+this.props.opp.forEach(op => {
+  causes.push(op.cause);
+});
+
+let causeOptions = _.uniq(causes).map((cause,index) => {
+        return (
+          <label class="container" key={index}>{cause}
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+        )})
+
+    return (
+      <div>
+        <label> Choose Your Cause </label>
+           <div> {causeOptions} </div>
+        <label> Choose Your Time </label>
+           <Input
+                  name="start_date"
+                  type="date"
+                  value={this.state.start_date}
+                  onChange={this.updateInput} />
+            <Input
+                  name="end_date"
+                  type="date"
+                  value={this.state.end_date}
+                  onChange={this.updateInput} />
+      </div>
+    );
+ }
+};
+
 
 export default Filter;
--- client/components/OpsList.jsx
+++ client/components/OpsList.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
+
 import Ops from './Ops.jsx';
+
 import { Container, Row, Col } from 'reactstrap';
+import Filter from './Filter.jsx';
+
+
 
 const OpsList = function(props) {
+
   return (
+
     <Container>
-      <Row> 
+
+      <Row>
+
         <Col xs="8">
+
           {props.opportunities.map(function(opportunity, idx) {
+
             return <Ops enroll={props.enroll} opportunity={opportunity} key={idx} />
+
           })}
+
         </Col>
+
         <Col xs="4">
+
+        <Filter opp = {props.opportunities} />
+
         </Col>
+
         </Row>
+
     </Container>
+
   );
+
 };
+
+
 
 export default OpsList;
