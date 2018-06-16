a21b852115408aeaf7364d17e88f549d35040da6
Don de Rojas <derojasdonny@gmail.com>
2018-06-15 19:56:28 -0700

    testing private rooms for socket.io

--- index.js
+++ index.js
@@ -6,16 +6,37 @@ var server = app.listen(port, function() {
   console.log(`Listening on ${port}`);
 });
 const socketIO = require('socket.io')(server);
+var nsp = socketIO.of('/jabroni');
 
+nsp.on('connection', function(socket) {
+  console.log('socket connected jabroni!');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    nsp.emit('chat message', msg);
+  });
+  socket.on('disconnect', function (data) {
+  console.log('socket disconnected jabroni!', data);
+  });
+});
 
 socketIO.on('connection', function(socket) {
-  console.log('socket connected!');
+  console.log('socket connected everyone!');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     socketIO.emit('chat message', msg);
   });
   socket.on('disconnect', function (data) {
-  console.log('socket disconnected!', data);
+  console.log('socket disconnected everyone!', data);
   });
 });
-// Namespace? Possibly to talk with specific administrations
+
+/*
+// Namespace or room socket ID? Possibly to talk with specific administrations
+// specific window to talk
+// to save or not save the chat
+// join leave rooms
+// put into react somehow
+Idea?
+
+  everytime a user is created a new namespace shoudl be created on the server side and client?
+  */
