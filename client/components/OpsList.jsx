a0dacecd739684f7f779cbb4f2ed54a29fa3565a
sam-shih <samuel.jh.shih@gmail.com>
2018-06-16 15:02:40 -0700

    Watch button/function working fullstack

--- client/components/OpsList.jsx
+++ client/components/OpsList.jsx
@@ -29,9 +29,7 @@ class OpsList extends React.Component {
           <Row>
             <Col md="12">
               {this.props.opportunities.map(op => {
-
                 return <Ops volunteerForOpp={this.props.volunteerForOpp} opportunity={op} isLoggedIn={this.props.isLoggedIn} user={this.props.user} key={op._id} watchOpp={this.props.watchOpp}/>
-
               })}
             </Col>
 
