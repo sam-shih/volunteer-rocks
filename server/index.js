4b08e8657f93f840540871187cdea2611086f93a
Don de Rojas <derojasdonny@gmail.com>
2018-06-17 14:07:29 -0700

    fixed package-lock.json issue

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ const VolunteerModel = require('../database/models.js').Volunteers;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const exampleDB = require('../database/exampleOpGenarator.js');
 const addVolunteerToOpp = require('../database/addVolunteerToOpp').checkIfEnrolled;
-<<<<<<< HEAD
-=======
+
 const saveToDb = require('../database/saveToDb.js');
 const checkdb = require('../database/checkdbs.js');
 
